24e43dbfe578a741e3792205f3e0f605f02fcaae
Douglas Lonkoski <douglaslonkoski6@hotmail.com>
2020-05-03 16:29:59 -0300

    Start Commmit

---
+++ app/src/main/java/com/lonkoski/caraoucoroa/MainActivity.kt
@@ -0,0 +1,33 @@
+
+//Douglas Lonkoski -  03/05/2020
+
+package com.lonkoski.caraoucoroa
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        image_jogar.setOnClickListener {
+
+            val intent = Intent(applicationContext, ResultadoActivity::class.java)
+
+            //Gerar numero randômico
+
+            val numero = Random().nextInt(2)
+
+            //Enviar numero para outra activity
+
+            intent.putExtra("numero", numero)
+
+            startActivity(intent)
+        }
+    }
+}
